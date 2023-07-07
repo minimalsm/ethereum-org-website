54cd57bfcbfef6f6a681f127ae7dac4ddba9a178
Joshua <62268199+minimalsm@users.noreply.github.com>
2023-07-07 21:30:16 +0100

    Update fileHelpers.ts

--- src/scripts/crowdin/reports/fileHelpers.ts
+++ src/scripts/crowdin/reports/fileHelpers.ts
@@ -41,12 +41,17 @@ export async function saveReportDataToJson(
   const languageData = combinedData.find((data) => data.lang === language)
 
   if (languageData) {
-    languageData.data.push({
-      fileId: fileId.toString(),
-      contributors: formattedData,
-    })
+    const existingData = languageData.data.find((data) => data.fileId === fileId.toString())
+    if (existingData) {
+      existingData.contributors = formattedData;
+    } else {
+      languageData.data.push({
+        fileId: fileId.toString(),
+        contributors: formattedData,
+      })
+    }
   } else {
-    combinedData!.push({
+    combinedData.push({
       lang: language,
       data: [
         {
@@ -61,10 +66,10 @@ export async function saveReportDataToJson(
     await fs.promises.writeFile(
       combinedFilePath,
       JSON.stringify(combinedData, null, 2)
-    )
+    );
   } catch (error: unknown) {
     if (error instanceof Error) {
-      console.log(`Error writing to ${combinedFilePath}:`, error.message)
+      console.log(`Error writing to ${combinedFilePath}:`, error.message);
     }
   }
 }
